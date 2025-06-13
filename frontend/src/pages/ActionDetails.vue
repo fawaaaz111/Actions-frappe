1fde9287de85433db63f5b3b1beb3e825a5fb6b2
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-06-13 16:28:15 +0300

    feat: apply GO Back when multi pages

--- frontend/src/pages/ActionDetails.vue
+++ frontend/src/pages/ActionDetails.vue
@@ -1,9 +1,15 @@
 <template>
-    <h1>action details: {{ props.name }}</h1>
+    <div>
+        <h1>action details: {{ props.name }}</h1>
+        <Button @click="router.back()">Go Back</Button>
+    </div>
 </template>
 
 <script lang="js" setup>
 // @ts-nocheck
+import { useRouter } from 'vue-router';
+import { Button } from 'frappe-ui';
 
+const router = useRouter();
 const props = defineProps(['name'])
 </script>
