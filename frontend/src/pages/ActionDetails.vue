77897eed5cfc5839e39a2300fff760588e88f012
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-06-16 00:24:43 +0300

    feat: enhance the Action Details page

--- frontend/src/pages/ActionDetails.vue
+++ frontend/src/pages/ActionDetails.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
-        <h1>action details: {{ props.name }}</h1>
-        <Button @click="router.back()">Go Back</Button>
+        <div class = "mx-20 my-4" v-if="!action.get.loading">
+            <h1 class="font-black text-5xl text-gray-900">{{ action.doc.title }}</h1>
+            <Button @click="router.back()">Go Back</Button>
+        </div>
+        <LoadingIndicator class="w-6 text-center text-blue-500" v-else/>
     </div>
 </template>
 
 <script lang="js" setup>
 // @ts-nocheck
 import { useRouter } from 'vue-router';
-import { Button } from 'frappe-ui';
+import { Button, createDocumentResource, LoadingIndicator } from 'frappe-ui';
 
 const router = useRouter();
 const props = defineProps(['name'])
+
+const action = createDocumentResource({
+    doctype: 'Action',
+    name: props.name,
+});
 </script>
